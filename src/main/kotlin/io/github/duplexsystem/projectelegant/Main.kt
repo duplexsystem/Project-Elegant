42078b564c9bbb0ce821bfe778b198cb9025c8c5
Zoë <duplexsys@protonmail.com>
2021-09-30 19:00:33 -0500

    Main class

--- src/main/kotlin/io/github/duplexsystem/projectelegant/Main.kt
+++ src/main/kotlin/io/github/duplexsystem/projectelegant/Main.kt
@@ -1,8 +1,10 @@
+package io.github.duplexsystem.projectelegant
+
 class Main {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            io.github.duplexsystem.projectelegant.Main.main(args)
+
         }
     }
 }
